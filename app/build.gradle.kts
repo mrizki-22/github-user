a20411a16885f62bbf1f2748ab8e195e306094e8
mrizki-22 <mrizkibgr22@gmail.com>
2023-09-16 12:31:52 +0700

    feat: add setting preference

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,9 +57,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.viewpager2:viewpager2:1.0.0")
-
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
     implementation("androidx.cardview:cardview:1.0.0")
-
     implementation("androidx.room:room-runtime:2.5.2")
     kapt ("androidx.room:room-compiler:2.5.2")
     testImplementation("junit:junit:4.13.2")
